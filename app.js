befa1752209d9486191d2fefcc42a995437b3652
Soo Yeon Kim <104661679+ooskal@users.noreply.github.com>
2022-09-18 11:26:27 +0900

    html

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -11,8 +13,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next)=>{
-    res.status(404).send('<h1>page not found</h1>');
+app.use((req, res, next)=> {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
