69ceb3c068628d1956f8124b1ea169371ba77939
Soo Yeon Kim <104661679+ooskal@users.noreply.github.com>
2022-10-07 16:32:41 +0900

    cart

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,11 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productsController = require("../controllers/products");
 
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    console.log(adminData.products);
-    res.sendFile(path.join(rootDir,'views', 'shop.html')); 
-});
+router.get("/", productsController.getProducts);
 
 module.exports = router;
